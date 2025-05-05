6ec6b56fdd36526fd02725a0c67441db41b727f2
Joshua <joshloufek@gmail.com>
2025-05-05 14:21:20 -0400

    Animation and Movement Framework

--- Assets/Squirrel Scramble/Scripts/AnimatedSprite.cs
+++ Assets/Squirrel Scramble/Scripts/AnimatedSprite.cs
@@ -15,4 +15,31 @@ public class AnimatedSprite : MonoBehaviour
         this.spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
+    private void Start()
+    {
+        InvokeRepeating(nameof(Advance), this.animationTime, this.animationTime);
+    }
+
+    private void Advance()
+    {
+        if (!this.spriteRenderer.enabled) {
+            return;
+        }
+
+        this.animationFrame++;
+
+        if (this.animationFrame >= this.sprites.Length && this.loop) {
+            this.animationFrame = 0;
+        }
+
+        if (this.animationFrame >= 0 && this.animationFrame < this.sprites.Length) {
+            this.spriteRenderer.sprite = this.sprites[this.animationFrame];
+        }
+    }
+
+    public void Restart()
+    {
+        this.animationFrame = -1;
+        Advance();
+    }
 }
---
+++ Assets/Squirrel Scramble/Scripts/Movement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D myRB { get; private set; }
+
+    public float speed = 8.0f;
+    public float speedMultiplier = 1f;
+    
+    public LayerMask obstacleLayer;
+
+    public Vector2 initialDirection;
+    public Vector2 direction { get; private set; } // The direction the object is currently moving.
+    public Vector2 nextDirection { get; private set; } // The direction the player can queue for when they reach the next junction.
+    public Vector3 startPosition { get; private set; }
+
+    private void Awake()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        ResetState();
+    }
+
+    public void ResetState()
+    {
+        speedMultiplier = 1f;
+        direction = initialDirection;
+        nextDirection = Vector2.zero;
+        transform.position = startPosition;
+        myRB.isKinematic = false;
+        enabled = true;
+    }
+
+    private void Update()
+    {
+        // Every update this entity should attempt to move in its intended "nextDirection"
+        if (nextDirection != Vector2.zero) {
+            SetDirection(nextDirection);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = myRB.position;
+        Vector2 translation = direction * speed * speedMultiplier * Time.fixedDeltaTime;
+        myRB.MovePosition(position + translation);
+    }
+    
+    public void SetDirection(Vector2 newDirection, bool force = false)
+    {
+        if(force || !Occupied(newDirection))
+        {
+            direction = newDirection;
+            nextDirection = Vector2.zero;
+        }
+        else // case when the attempted direction is blocked
+        {
+            nextDirection = newDirection; // Set the intended direction as the "nextDirection" the object will attempt to move.
+        }
+    }
+
+    public bool Occupied(Vector2 newDirection)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0.0f, newDirection, 1.5f, obstacleLayer);
+        return hit.collider != null;
+    }
+}
